4442562c5262b15f4c4f0e84b104c374a6d0f2d3
MatthewLZR <mlopez@re7systems.com>
2021-12-30 12:21:18 +0100

    Documentacio amb swagger

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -5,14 +5,35 @@ import { Request } from 'express';
 import { BookDto } from './dto/Books.dto';
 import { Book } from './book.entity'; 
 import { AuthGuard } from '@nestjs/passport';
+import { 
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 
+@ApiTags('book') 
 @Controller('books')
-
+@UseGuards(AuthGuard('jwt')) 
+@ApiBearerAuth('access-token')
 @UseGuards(AuthGuard('jwt'))
+
 export class BooksController {
 
   constructor(private booksService: BooksService) {}
-
+/** 
+   *
+   * @returns {Book[]} Devuelve una lista de libros
+   * @param {Request} request Lista de parámetros para filtrar
+   */
+ @Get()
+ @ApiOperation({ summary: 'Obtener lista de libros' }) 
+ @ApiResponse({ 
+   status: 201,
+   description: 'Lista de libros',
+   type: Book, 
+ })
+ 
   @Get()
   findAll(@Req() request: Request): Promise<Book[]> { 
     console.log(request.query);
